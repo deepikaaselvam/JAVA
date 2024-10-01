63ab82303cdfe9282da41afa8b144787f1aa3ff8
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-10-01 20:51:45 +0530

    Finding the Sqrt of the Number using BSearch

---
+++ Sqrt2.java
@@ -0,0 +1,41 @@
+public class Sqrt2 {
+    public static int  BSSquareRoot(long n)
+    {
+        if(n==0)
+        {
+            return (int)n;
+        }
+        if(n==1)
+        {
+            return (int)n;
+        }
+        long l=1 ,r=n;
+        int ans=1;
+        while(l<=r)
+        {
+            long mid=(l+(r-l)/2);  ///When we take integer datatype,for largest value,It cannot calculate the "mid" value
+                                    //So that ,We take all the values as "long" datatype,It will calculate the Correct Valuesand Give the Correct Output
+            long multipliedValue=mid*mid;
+            if(multipliedValue==n)
+            {
+                return (int)mid;
+            }
+            if((multipliedValue)<=n)
+            {
+                ans=(int)mid;// The ans variable can might be our answer
+                l=mid+1;
+                
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        long n=2147395599L;//28   //512
+        int answer=BSSquareRoot(n);
+        System.out.println(answer);
+    }
+    
+}
